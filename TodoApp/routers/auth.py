07c74f570d6a85697d19444b64a4153904f7f9fa
Ana Elles <alelles16@gmail.com>
2024-11-28 15:12:18 -0500

    feat: Add authentica method for validating user info

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,70 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from typing import Annotated
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from models import Users
 
 router = APIRouter()
 
 
-@router.get('/auth')
-async def get_user():
-    return {'user': 'authenticated'}
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
+@router.post('/auth', status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+    db.add(create_user_model)
+    db.commit()
+
+
+@router.post('/token')
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: db_dependency
+    ):
+    user = authenticate_user(username=form_data.username, password=form_data.password, db=db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful Authentication'
